135cda09bb72257b6562d4ea51bf135513bd3f3d
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-03-07 17:53:36 +0530

    initial commit

---
+++ Login_page/src/test/java/com/example/Login_page/LoginPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Login_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
